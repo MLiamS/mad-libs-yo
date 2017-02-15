a463951e52bce916e9769551d7729b83c8a33d9d
Grace Stuart and Ryan Jones <grace@>
2017-02-15 09:05:38 -0800

    add funtion to submit the form.

---
+++ js/madLib.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(){
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput =$("input#noun").val();
+  });
+});

72963b3a580204fffa8a6e427d13cc0ddebeaf50
Grace Stuart and Ryan Jones <ryan@>
2017-02-15 09:56:18 -0800

    content to the letter.

--- js/letter.js
+++ js/letter.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-  var personInput = $("#person1").val();
-  var streetInput = $("#street").val();
-  var cityInput = $("#city").val();
-  var stateInput = $("#state").val();
-  var zipInput = $("#zip").val();
+  $("#formOne").submit(function() {
+    var personInput = $("#person1").val();
+    var streetInput = $("#street").val();
+    var cityInput = $("#city").val();
+    var stateInput = $("#state").val();
+    var zipInput = $("#zip").val();
 
-  $("#letter").show();
-  event.preventDefault();
+    $(".name").text(personInput);
+    $(".street").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
+
+    $("#letter").show();
+    event.preventDefault();
+  });
 });

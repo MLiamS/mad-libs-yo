4b7876ab5106dbc6ba6f93d963cd35659237bbae
Grace Stuart and Ryan Jones <grace@>
2017-02-15 09:57:51 -0800

    add function to hide form on submit.click.

--- js/letter.js
+++ js/letter.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
     $(".state").text(stateInput);
     $(".zip").text(zipInput);
 
+    $("#formOne").hide();
     $("#letter").show();
     event.preventDefault();
   });
